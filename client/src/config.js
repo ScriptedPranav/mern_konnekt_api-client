2853d26e5c9084b2b4116ead3436439296397440
ScriptedPranav <sinhafighterpranav@gmail.com>
2022-12-04 16:57:50 +0530

    build for render

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL : "https://konnekt-social.herokuapp.com/api/"
+    baseURL : "https://konnekt-social.onrender.com/api/"
 })
