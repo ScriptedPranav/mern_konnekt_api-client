fd223e9ee5261562417f3a77d2c5b1748f03ca0d
ScriptedPranav <sinhafighterpranav@gmail.com>
2022-12-04 21:07:49 +0530

    fix /

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -18,7 +18,7 @@ export default function Rightbar({ user }) {
   const socket = useRef();
 
   useEffect(() => {
-    socket.current = io("ws://https://konnekt-socket-server.onrender.com");
+    socket.current = io("ws://https://konnekt-socket-server.onrender.com/");
   }, []);
 
   useEffect(() => {
